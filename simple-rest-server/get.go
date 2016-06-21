df17d3758751993f6fc57e626a6d51cf8daa167d
Nichelle A Hall <nahall@nahall-mac01.jf.intel.com>
2016-06-21 14:54:16 -0700

    Edited the ListTask function to return JSON file.

--- simple-rest-server/get.go
+++ simple-rest-server/get.go
@@ -3,40 +3,14 @@ package main
 import (
 	"fmt"
 	"net/http"
-    "os"
-	"encoding/csv"
+	"encoding/json"
 	"github.com/julienschmidt/httprouter"
 )
 
 func ListTask(respWriter http.ResponseWriter, request *http.Request, params httprouter.Params) {
-	 csvfile, err := os.Open("tasks.csv")
 
-         if err != nil {
-                 fmt.Println(err)
-                 return
-         }
-
-         defer csvfile.Close()
-
-         reader := csv.NewReader(csvfile)
-
-         reader.FieldsPerRecord = -1 
-         
-         rawCSVdata, err := reader.ReadAll()
-
-         if err != nil {
-                 fmt.Println(err)
-                 os.Exit(1)
-         }
-
-         // sanity check, display to standard output
-         for _, each := range rawCSVdata {
-             status := "unchecked"
-             if each[1] == "true" {  // each[1] will be the status - either true or false
-                 status = "checked"
-             }
-              fmt.Fprintln(respWriter,"ID:", each[0], "Status :", status, "Date Added:", each[2], "Date Due:", each[3], "Task:", each[4])
-         }
+	output, _ := json.MarshalIndent(allTasks, "", "  ")
+    fmt.Fprintln(respWriter, string(output))
 }
 
 func SearchTask(respWriter http.ResponseWriter, request *http.Request, params httprouter.Params) {
