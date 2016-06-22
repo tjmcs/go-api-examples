f42f6f8c24297e007a183314ab350dc3c99dc146
zachary-trudo <zachary.a.trudo@intel.com>
2016-06-22 11:34:51 -0700

    Fixed bugs in delete functionality.

--- simple-rest-server/delete.go
+++ simple-rest-server/delete.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/julienschmidt/httprouter"
 	"io/ioutil"
 	"log"
@@ -24,17 +25,31 @@ func DeleteTask(respWriter http.ResponseWriter, request *http.Request, _ httprou
 		log.Println(err)
 	}
 
-	removeTaskByID(remove.ID)
+	index, err := removeTaskByID(remove.ID)
+	if index == -1 {
+		fmt.Fprintln(respWriter, "Task ID not found. Unable to delete")
+	} else {
+		fmt.Fprintln(respWriter, "Task ID: "+string(index)+" found.")
+	}
 }
 
-func removeTaskByID(taskID int) (err error) {
+func removeTaskByID(taskID int) (int, error) {
 	index, err := getIndexByTaskID(taskID)
 	if err != nil {
 		log.Println(err)
-		return
+		return index, err
 	}
+
 	accessTasks.Lock()
-	allTasks = append(allTasks[:index-1], allTasks[index+1:]...)
+	if index == -1 {
+		return index, err
+	} else if index == 0 {
+		allTasks = allTasks[1:]
+	} else if index == len(allTasks)-1 {
+		allTasks = allTasks[:len(allTasks)-1]
+	} else {
+		allTasks = append(allTasks[:index-1], allTasks[index+1:]...)
+	}
 	accessTasks.Unlock()
-	return
+	return index, err
 }
